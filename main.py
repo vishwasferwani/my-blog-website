eb4fdbd850cb39091e6420edce588483f00979d5
Vishwas Ferwani <166019560+vishwasferwani@users.noreply.github.com>
2024-10-26 16:59:36 +0530

    Update main.py
    sorted the blog in latest post first

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def logout():
 
 @app.route('/')
 def get_all_posts():
-    result = db.session.execute(db.select(BlogPost))
+    result = db.session.execute(db.select(BlogPost).order_by(BlogPost.date)
     posts = result.scalars().all()
     return render_template("index.html", all_posts=posts,current_user=current_user)
 
